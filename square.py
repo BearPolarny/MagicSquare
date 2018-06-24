a2fccde3ee7dbe1fd36bb014021b5b204aef0d9b
BearPolarny <shuller464@gmail.com>
2018-06-24 02:08:08 +0200

    MagicSquare v0.1

---
+++ square.py
@@ -0,0 +1,64 @@
+from sys import argv
+
+from numpy import sum, zeros
+
+
+def generate_square(val):
+    magicSquare = zeros((val, val))
+
+    val_s = val ** 2
+
+    i = int(val / 2)
+    j = val - 1
+
+    num = 1
+    while num <= val_s:
+        if i == -1 and j == val:
+            j = val - 2
+            i = 0
+        else:
+            if j == val:
+                j = 0
+            if i < 0:
+                i = val - 1
+
+        if magicSquare[i, j]:
+            j = j - 2
+            i = i + 1
+            continue
+        else:
+            magicSquare[i, j] = num
+            num += 1
+
+        j = j + 1
+        i = i - 1
+    return magicSquare.astype(int)
+
+
+def check_square(square):
+    x = len(square)
+
+    control_sum = sum(square[0])
+
+    diag1 = 0
+    diag2 = 0
+
+    for i in range(x):
+        magic = sum(square[i]) == control_sum
+        if not magic:
+            return False, -1
+        magic = sum(square[:, i]) == control_sum
+        if not magic:
+            return False, -1
+        diag1 += square[i, i]
+        diag2 += square[i, x - i - 1]
+
+    magic = diag1 == diag2 == control_sum
+    if magic:
+        return magic, control_sum
+    else:
+        return magic, -1
+
+
+if __name__ == '__main__':
+    print(' ' + str(generate_square(int(argv[1]))).replace("[", '').replace(']', ''))
